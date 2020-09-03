e392e36bd87d954f6fddfd5a96e230ae21be07b9
Sharika Nahid <nahidsharika@gmail.com>
2020-09-03 23:50:20 +0600

    add example 3-2

---
+++ chapter_3_exploring_php/example-3-2.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Example 3-2 : A wrong way to add some PHP code to HTML file
+ */
+?>
+<html>
+<head>
+    <title>Hello World!</title>
+</head>
+<body>
+echo "<p> Now I have to something to say </p>"
+</body>
+</html>
