91019fa72db68d3adb1cf0afd554029e022128d3
Sharika Nahid <nahidsharika@gmail.com>
2020-09-03 23:43:55 +0600

    add example 3-1

---
+++ chapter_3_exploring_php/example-3-1.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Example 3-1 : All you need to start with PHP is a simple HTML document.
+ */
+?>
+<html>
+<head>
+    <title>Hello World!</title>
+</head>
+<body>
+<p>I sure wish I had something to say.</p>
+</body>
+</html>
