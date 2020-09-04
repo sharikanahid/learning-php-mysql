e4bd01c06b441544be5a1da0e277af56bde4aed5
Sharika Nahid <nahidsharika@gmail.com>
2020-09-04 20:43:32 +0600

    add example 3-3

---
+++ chapter_3_exploring_php/example-3-3.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Example 3-3: Correctly calling print in hello.php
+ */ ?>
+<html>
+<head>
+    <title> Hello World!</title>
+</head>
+<body>
+
+<?php
+print "Hello world! <br/>";
+print "Goodbye <br/>";
+print "Over and out <br/>";
+?>
+
+</body>
+</html>
