e0f0a6632ed9861f0b05f9d2e18b7b41826ea19a
Sharika Nahid <nahidsharika@gmail.com>
2020-09-06 21:48:49 +0600

    add example 3-5

---
+++ chapter_3_exploring_php/example-3-5.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Example 3-5: Using comments to make your code easier to read
+ *
+ */
+?>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+<?php
+//A single line comment could say that we are going to
+// print hello world.
+/* This is how to do a
+multiline comment and could be used to comment out a block
+of code */
+echo "Hello world! <br/>";
+echo "Goodbye <br/>";
+?>
+
+</body>
+</html>
