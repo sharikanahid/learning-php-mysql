73be3fcd7f6920edc74b96335ba7004c7ec330c9
Sharika Nahid <nahidsharika@gmail.com>
2020-09-06 21:46:48 +0600

    add example 3-4

---
+++ chapter_3_exploring_php/example-3-4.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Example-3-4: The HTML markup produced by the PHP code in example-3-3
+ */
+?>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+Hello World <br/> Goodbye <br/> Over and out
+</body>
+</html>
