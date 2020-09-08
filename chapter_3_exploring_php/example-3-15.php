679d279a31a768c50a5300c16fc2f57130784e2f
Sharika Nahid <nahidsharika@gmail.com>
2020-09-08 23:26:55 +0600

    add example 3-15

---
+++ chapter_3_exploring_php/example-3-15.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Example-3-15:Breaking echo with special characters
+ */
+//This won't work because of the quotes around special H2!
+echo "<h2 class="specialH2">Margaritaville! </h2>";
+?>
+specialH2
