4311be8ade71123b9ad338a048389a30b46489a8
Sharika Nahid <nahidsharika@gmail.com>
2020-09-09 00:26:04 +0600

    add example 3-18

---
+++ chapter_3_exploring_php/example-3-18.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Example-3-18: Concatenating strings together
+ */
+$my_string = "Hello Max. My name is:";
+$newline = "<br/>";
+echo $my_string . "Paula" . $newline;
+echo "Hi, I'm Max.Who are you?" . $my_string . $newline;
+echo "Hi, I'm Max.Who are you?" . $my_string . "Paula";
+//The last line is the same as echo "Hi, I'm Max.WHo are you? $my_string Paula";
+?>
