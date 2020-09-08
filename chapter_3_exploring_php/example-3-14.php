03f7fffb7c4c62daec53e9cc073f257d2b4f91b5
Sharika Nahid <nahidsharika@gmail.com>
2020-09-08 23:23:32 +0600

    add example 3-14

---
+++ chapter_3_exploring_php/example-3-14.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Example-3-14: Various special characters in string assignment
+ */
+$newline = "A newline is \n";
+$return = "A carriage return is \r";
+$tab = "A tab is \t";
+$dollar = "A dollar is \$";
+$doublequote = "A double-quote is \"";
+echo $newline;
+?>
